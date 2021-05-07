a542a67ab46cf79d236ac980e5061de4ae31970f
cameron norman <cnorm001@odu.edu>
2021-05-07 11:19:12 -0400

    Refactoring code base.

--- Gui.py
+++ Gui.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.simpledialog
+import TweetMiner
 
 class Gui(tk.Frame):
     def __init__(self, master = None):
@@ -9,9 +10,23 @@ class Gui(tk.Frame):
     def init_window(self):
         self.master.title("Tweet Miner")
         self.pack(fill=tk.BOTH, expand=1)
-
         twitterUsername = tk.simpledialog.askstring(title="Enter A Twitter Username", prompt='Please enter a twitter username')
         
+        collect_tweets_button = tk.Button(self, text = "Collect Tweets", fg = "black" , bg = "gray", command = lambda:[self.request_number_of_tweets_to_collect(), tweetMiner.getTweets(twitterUsername, numberOfTweetsToCollect)])
+        collect_tweets_button.place(relx = 0.5, rely = 0.5, anchor = tk.CENTER)
+        collect_tweets_button.config(height = 4, width = 20)
+    
+    def request_number_of_tweets_to_collect(self):
+        
+        while True:
+            try:
+                numberOfTweetsToCollect = int(tk.simpledialog.askstring(title="How many Tweets?", prompt='Please enter the # of tweets to collect'))
+                break
+            except ValueError:
+                print("Please enter a valid number!")
+                
+        return numberOfTweetsToCollect
+   
     def exit(self):
         exit()
 
--- TweetMiner.py
+++ TweetMiner.py
@@ -5,21 +5,25 @@ except ImportError:
     import urllib2
 
 class TweetMiner:
-    def __init__(self, consumer_key = None, consumer_secret = None, access_token = None, access_token_secret = None):
+    def __init__(self, consumer_key = None, consumer_secret = None, access_token = None, access_token_secret = None, api = None):
         self.consumer_key = consumer_key
         self.consumer_secret = consumer_secret
         self.access_token = access_token
         self.access_token_secret = access_token_secret
+        self.api = api
 
     def authenticate(self):
         # Handling authentication using tweepy OAuthHandler Method
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
-        api = tweepy.API(auth)
+        auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tweepy.API(auth)
         return self.api
 
-    def get_tweets(self, username):  
-        pass
+    def getTweets(self, username): 
+        self.api.user_timeline(screen_name = username, count = numberOfTweetsToCollect)
+    
+    def displayTweets(self):
+        pass  
 
 # Initializing consumeer key and secret as well as access token and secret. 
 consumer_key = os.environ.get('c_k')
